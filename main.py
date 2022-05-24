8d387d51545b4c4fd794c3aae835a8314be60a0e
Duoteng Xu <62707393+Xuduoteng@users.noreply.github.com>
2022-05-24 10:24:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import streamlit as st
 
 if __name__ == '__main__':
     st.title('Duoteng Xu’s Homepage')
+    st.set_page_config(
+     page_title="Ex-stream-ly Cool App",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+             'Get Help': 'https://www.extremelycoolapp.com/help',
+             'Report a bug': "https://www.extremelycoolapp.com/bug",
+             'About': "# This is a header. This is an *extremely* cool app!"
+         }
+     )
     st.image("./cv.jpg",use_column_width=True)
     #st.image("./cv1.jpg")
 
